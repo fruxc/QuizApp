81ed25e0ebc33a5ebb2312a768adc2da627900e9
Hammad Ansari <hammadansari17@gmail.com>
2021-09-09 16:06:51 +0530

    Deploy config

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ app.use((req, res, next) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, "client/build")));
   const path = require("path");
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
